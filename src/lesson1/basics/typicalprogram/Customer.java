682461e2e2c9106914cc7b0f81a058bc0414861d
Soueilem Mohamed Lemine <msoueilem@miu.edu>
2021-11-23 06:57:02 -0600

    Update Customer

--- src/lesson1/basics/typicalprogram/Customer.java
+++ src/lesson1/basics/typicalprogram/Customer.java
@@ -5,27 +5,33 @@ package lesson1.basics.typicalprogram;
  * @author Soueilem Mohamed Lemine
  */
 public class Customer {
-    private String name;
-    private String email;
-    private int orderNumber;
-    private int price;
-    public Customer(String na, String em, int ordNu, int pr){
-        name = na;
-        email = em;
-        orderNumber = ordNu;
-        price = pr;
+    private String firstName;
+    private String lastName;
+    private String socSecurityNum;
+    private Address billingAddress;
+    private Address shippingAddress;
+    public Customer(String fN, String lN, String sS){
+        firstName = fN;
+        lastName = lN;
+        socSecurityNum = sS;
     }
-    public String getName(){ return name;}
-    public String getEmail(){ return email;}
-    public int getOrderNumber(){ return orderNumber;}
-    public int getPrice(){ return price;}
+    public String getFirstName() { return firstName;}
+    public void setFirstName(String fn) { this.firstName = fn;}
+    public String getLastName() { return lastName;}
+    public void setLastName(String ln) { this.lastName = ln;}
+    public String getSocSecurityNum(){return socSecurityNum;}
+    public void setSocSecurityNum(String sc){ this.socSecurityNum = sc;}
+    public Address getBillingAddress(){return billingAddress;}
+    public void setBillingAddress(Address ba){ this.billingAddress = ba;}
+    public Address getShippingAddress(){return shippingAddress;}
+    public void setShippingAddress(Address sa){ this.shippingAddress = sa;}
 
     /**
      * Provides a string representation of an address.
      * @return a string representation of an addresss
      */
     public String toString() {
-        return "Customer Info\nName: "+ name + "\nEmail: " + email + "\nOrder Details\nNumber: "+ orderNumber + "\nPrice: $" + price;
+        return "Customer Info\nFirst Name: "+ firstName + "\nLast Name: " + lastName + "\nSSN: " + socSecurityNum + "\nAddress\nShipping Address: "+ shippingAddress + "\nBilling Address: " + billingAddress;
     }
 
 }
